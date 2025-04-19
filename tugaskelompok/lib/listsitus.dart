d01aadda1920feebb8322d6e811d6f3d1f6ce7c1
MorvyExpected <153181833+MuhammadOscarFileas@users.noreply.github.com>
2025-04-19 16:38:23 +0700

    Update listsitus.dart

--- tugaskelompok/lib/listsitus.dart
+++ tugaskelompok/lib/listsitus.dart
@@ -2,38 +2,38 @@ final List<Map<String, String>> menuItems = [
   {
     "judul": "Spada UPNYK",
     "link": "https://spada.upnyk.ac.id",
-    "image": "assets/piring.jfif"
+    "image": "assets/UPN.png"
   },
   {
     "judul": "Bima UPNYK",
     "link": "https://bima.upnyk.ac.id",
-    "image": "assets/piring.jfif"
+    "image": "assets/bima.png"
   },
   {
     "judul": "Sadewa UPNYK",
     "link": "https://sadewa.upnyk.ac.id",
-    "image": "assets/piring.jfif"
+    "image": "assets/sadewav2.png"
   },
   {
     "judul": "Nakula  UPNYK",
     "link": "https://nakula.upnyk.ac.id",
-    "image": "assets/piring.jfif"
+    "image": "assets/nakula.png"
   },
   {
     "judul": "Gacor 99 ",
-    "link": "https://images.app.goo.gl/3dvMjZTvPuknwVfN6",
+    "link": "https://bit.ly/dapat-uang-1jt",
     "image": "assets/piring.jfif"
   },
   {
     "judul": "Ngga gacor 777",
-    "link": "https://images.app.goo.gl/3dvMjZTvPuknwVfN6",
+    "link": "https://bit.ly/dapat-uang-1jt",
     "image": "assets/piring.jfif"
   },
   {"judul": "1CAK", "link": "https://1cak.com", "image": "assets/piring.jfif"},
   {
     "judul": "Lahelu",
     "link": "https://lahelu.com",
-    "image": "assets/piring.jfif"
+    "image": "assets/lahelu.png"
   },
   {
     "judul": "Facebook Arji",
